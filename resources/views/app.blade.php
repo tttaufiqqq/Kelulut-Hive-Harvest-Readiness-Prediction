bc52895c6d09512cc0e24f216714635ad3a58a27
tttaufiqqq <taufiq33992@gmail.com>
2026-05-20 00:54:02 +0800

    Revert "style: adopt Space Grotesk / Sora / JetBrains Mono typography from MetaMyKad"
    This reverts commit 5585cdc93f1bf5b505d4696507f6a79a83fb57c8.

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -42,9 +42,8 @@
         <link rel="icon" href="/favicon.ico" sizes="any">
         <link rel="apple-touch-icon" href="/icon-192.png">
 
-        <link rel="preconnect" href="https://fonts.googleapis.com">
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-        <link href="https://fonts.googleapis.com/css2?family=JetBrains+Mono:wght@500;700&family=Sora:wght@400;600;700;800&family=Space+Grotesk:wght@400;500;700&display=swap" rel="stylesheet">
+        <link rel="preconnect" href="https://fonts.bunny.net">
+        <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
 
         @routes
         @viteReactRefresh
